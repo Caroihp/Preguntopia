52f540f7d0fadf166cbf7dada7e2a0db7d5bf0b7
Caro.ihp <carolina.hp251014@gmail.com>
2023-12-18 03:43:38 -0300

    Avance

--- preguntopiam/lib/presentation/preguntasdeportes_screen/preguntasdeportes_screen.dart
+++ preguntopiam/lib/presentation/preguntasdeportes_screen/preguntasdeportes_screen.dart
@@ -1,75 +1,129 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:preguntopia/core/app_export.dart';
 import 'package:preguntopia/widgets/custom_elevated_button.dart';
 
-class PreguntasdeportesScreen extends StatelessWidget {
+class PreguntasdeportesScreen extends StatefulWidget {
   const PreguntasdeportesScreen({Key? key})
       : super(
           key: key,
         );
 
   @override
+  _PreguntasdeportesScreenState createState() => _PreguntasdeportesScreenState();
+}
+
+class Question {
+  String pregunta;
+  List<String> opciones;
+  String respuesta_correcta;
+
+  Question({required this.pregunta, required this.opciones, required this.respuesta_correcta});
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      pregunta: json['pregunta'],
+      opciones: List<String>.from(json['opciones']),
+      respuesta_correcta: json['respuesta_correcta'],
+    );
+  }
+}
+
+class _PreguntasdeportesScreenState extends State<PreguntasdeportesScreen> {
+  late List<Question> pregunta;
+  late Question currentQuestion;
+  late Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    loadQuestions().then((loadedQuestions) {
+      pregunta = loadedQuestions;
+      currentQuestion = pregunta[random.nextInt(pregunta.length)];
+      setState(() {});
+    });
+  }
+
+  Future<List<Question>> loadQuestions() async {
+    final jsonString = await rootBundle.loadString('assets/question/preguntas_deportes.json');
+    final jsonResponse = json.decode(jsonString) as List;
+    return jsonResponse.map((json) => Question.fromJson(json)).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
 
     return SafeArea(
       child: Scaffold(
         extendBody: true,
         extendBodyBehindAppBar: true,
         backgroundColor: appTheme.blueGray10001,
-        body: Container(
-          width: mediaQueryData.size.width,
-          height: mediaQueryData.size.height,
-          decoration: BoxDecoration(
-            color: appTheme.blueGray10001,
-            image: DecorationImage(
-              image: AssetImage(
-                ImageConstant.imgPreguntashistoria,
-              ),
-              fit: BoxFit.cover,
-            ),
+        body: pregunta.isNotEmpty 
+            ? buildQuestionContent(context, mediaQueryData) 
+            : Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+
+  Widget buildQuestionContent(BuildContext context, MediaQueryData mediaQueryData) {
+    return Container(
+      width: mediaQueryData.size.width,
+      height: mediaQueryData.size.height,
+      decoration: BoxDecoration(
+        color: appTheme.blueGray10001,
+        image: DecorationImage(
+          image: AssetImage(
+            ImageConstant.imgPreguntashistoria,
           ),
-          child: SizedBox(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                _buildTwo(context),
-                SizedBox(height: 9.v),
-                _buildEight(context),
-                Spacer(
-                  flex: 35,
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    width: 287.h,
-                    margin: EdgeInsets.only(right: 57.h),
-                    child: Text(
-                      "¿Dónde se encuentra \nel río Nilo?",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: theme.textTheme.headlineLarge,
-                    ),
-                  ),
-                ),
-                Spacer(
-                  flex: 64,
-                ),
-                _buildAsiaButton(context),
-                SizedBox(height: 17.v),
-                _buildEuropaButton(context),
-                SizedBox(height: 17.v),
-                _buildAfricaButton(context),
-                SizedBox(height: 17.v),
-                _buildAmericaButton(context),
-                SizedBox(height: 88.v),
-              ],
-            ),
-          ),
+          fit: BoxFit.cover,
         ),
       ),
+      child: SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _buildTwo(context),
+            SizedBox(height: 9.v),
+            _buildEight(context),
+            Spacer(flex: 35),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                width: 287.h,
+                margin: EdgeInsets.only(right: 57.h),
+                child: Text(
+                  currentQuestion.pregunta, // Usar la pregunta actual
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: theme.textTheme.headlineLarge,
+                ),
+              ),
+            ),
+            Spacer(flex: 64),
+            for (var opcion in currentQuestion.opciones) // Crear botones para las opciones
+              _buildOptionButton(context, opcion),
+            SizedBox(height: 88.v),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionButton(BuildContext context, String opcion) {
+    return CustomElevatedButton(
+      text: opcion,
+      margin: EdgeInsets.only(
+        left: 47.h,
+        right: 48.h,
+      ),
+      onPressed: () {
+        // Aquí manejas lo que sucede cuando se selecciona una opción
+      },
     );
   }
 
@@ -132,51 +186,6 @@ class PreguntasdeportesScreen extends StatelessWidget {
             style: theme.textTheme.titleSmall,
           ),
         ],
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildAsiaButton(BuildContext context) {
-    return CustomElevatedButton(
-      text: "Asia",
-      margin: EdgeInsets.only(
-        left: 47.h,
-        right: 48.h,
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildEuropaButton(BuildContext context) {
-    return CustomElevatedButton(
-      text: " Europa",
-      margin: EdgeInsets.only(
-        left: 47.h,
-        right: 48.h,
-      ),
-      buttonTextStyle: theme.textTheme.headlineLarge!,
-    );
-  }
-
-  /// Section Widget
-  Widget _buildAfricaButton(BuildContext context) {
-    return CustomElevatedButton(
-      text: "África",
-      margin: EdgeInsets.only(
-        left: 47.h,
-        right: 48.h,
-      ),
-    );
-  }
-
-  /// Section Widget
-  Widget _buildAmericaButton(BuildContext context) {
-    return CustomElevatedButton(
-      text: " América",
-      margin: EdgeInsets.only(
-        left: 47.h,
-        right: 48.h,
       ),
     );
   }
